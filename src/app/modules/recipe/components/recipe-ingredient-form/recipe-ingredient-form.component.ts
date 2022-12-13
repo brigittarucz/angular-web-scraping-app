485ca3b07c2ee5fd9620da0064a948f41fe3bac9
brigss <brigitta.rucz@yahoo.com>
2022-12-13 07:32:04 +0100

    Add form with input link

--- src/app/modules/recipe/components/recipe-ingredient-form/recipe-ingredient-form.component.ts
+++ src/app/modules/recipe/components/recipe-ingredient-form/recipe-ingredient-form.component.ts
@@ -1,21 +1,74 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-ingredient-form',
   template: `
-    Link Ingredient Website:
-    <input type="text" name="link" [formControl]="link" (keyup)="getInput()" />
+    <p>Insert below the link to the website to extract ingredients from:</p>
+    <form [formGroup]="formGroup" (ngSubmit)="onSubmit()">
+      <input
+        type="url"
+        name="url"
+        pattern="https://.*"
+        formControlName="url"
+        list="supportedUrls"
+        (focus)="onFocusIn()"
+        (focusout)="onFocusOut()"
+      />
+
+      <datalist id="supportedUrls">
+        <option value="https://developer.mozilla.org/"></option>
+      </datalist>
+
+      <div
+        *ngIf="
+          !isInputFocused &&
+          !formGroup.controls['url'].pristine &&
+          formGroup.controls['url'].errors
+        "
+        [ngStyle]="{ color: 'red', 'font-weight': 700 }"
+      >
+        <div *ngIf="formGroup.controls['url'].errors['required']">
+          <p>Please insert a value in the url field</p>
+        </div>
+
+        <div *ngIf="formGroup.controls['url'].errors['pattern']">
+          <p>Please add an https based url</p>
+        </div>
+      </div>
+
+      <button type="submit">Get ingredients</button><br />
+
+      <div
+        *ngIf="isFormSubmitted && this.formGroup.status === 'INVALID'"
+        [ngStyle]="{ color: 'red', 'font-weight': 700 }"
+      >
+        <p>Cannot submit form until errors are handled</p>
+      </div>
+    </form>
+    <small>Currently supported: xyz.recipes.com</small>
   `,
 })
 export class RecipeIngredientFormComponent {
   // Multiple values
-  // formGroup: FormGroup = new FormGroup({
-  //   link: new FormControl('', [Validators.required]),
-  // });
-  link = new FormControl('', [Validators.required]);
+  formGroup: FormGroup = new FormGroup({
+    url: new FormControl('', [Validators.required]),
+  });
+  isInputFocused: boolean = false;
+  isFormSubmitted: boolean = false;
 
-  getInput() {
-    console.log(this.link.value);
+  onSubmit() {
+    if (this.formGroup.status === 'INVALID') {
+      this.isFormSubmitted = true;
+      return;
+    }
+  }
+
+  onFocusIn() {
+    this.isInputFocused = true;
+  }
+
+  onFocusOut() {
+    this.isInputFocused = false;
   }
 }
--- src/app/modules/recipe/recipe.module.ts
+++ src/app/modules/recipe/recipe.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RecipeImportFormComponent } from './recipe.component';
 import { RecipeIngredientFormComponent } from './components/recipe-ingredient-form/recipe-ingredient-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [RecipeImportFormComponent, RecipeIngredientFormComponent],
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   providers: [],
   bootstrap: [],
 })
